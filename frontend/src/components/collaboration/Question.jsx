8f0bce3eca175f722fead8fcdc130a4a317968b2
Rayson <122922486+raysonchia@users.noreply.github.com>
2024-10-25 05:10:12 +0800

    Display question category and complexity

--- frontend/src/components/collaboration/Question.jsx
+++ frontend/src/components/collaboration/Question.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import "./styles/question.css";
 
 const Question = ({ question }) => {
   return (
-    <div>
+    <div className='question-tab'>
       <h3>{question.title}</h3>
+      <div className="tags">
+        <div className="complexity">{question.complexity}</div>
+        {
+          question.category.map((cat) => (
+            <div className="category">{cat}</div>
+          ))
+        }
+      </div>
       <p>{question.description}</p>
     </div>
   );
