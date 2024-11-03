6c8e3ddb143ef0a4e1acaa63a1c5b00bc3cdbdec
Yskie <ysk9680@gmail.com>
2024-11-03 22:34:21 +0800

    Fix default indentation code

--- frontend/src/pages/WaitingPage.jsx
+++ frontend/src/pages/WaitingPage.jsx
@@ -71,24 +71,24 @@ class Solution {
     } else {
       defaultCodes = {
         javascript: `// JavaScript code
-  const example = "raesa";
-  console.log(example);`,
+const example = "raesa";
+console.log(example);`,
     
         python: `# Python code
-  def main():
-      example = "raesa"
-      print(example)
+def main():
+    example = "raesa"
+    print(example)
 
-  if __name__ == "__main__":
-      main()`,
+if __name__ == "__main__":
+    main()`,
     
         java: `// Java code
-  public class Main {
-    public static void main(String[] args) {
-      String example = "raesa";
-      System.out.println(example);
-    }
-  }`
+public class Main {
+  public static void main(String[] args) {
+    String example = "raesa";
+    System.out.println(example);
+  }
+}`
       };
     }
 
