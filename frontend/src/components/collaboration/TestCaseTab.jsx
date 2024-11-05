e08a3131d852c58d116b285039c9b35816a02376
Rayson <122922486+raysonchia@users.noreply.github.com>
2024-11-05 21:51:35 +0800

    Add expected output to test case tab

--- frontend/src/components/collaboration/TestCaseTab.jsx
+++ frontend/src/components/collaboration/TestCaseTab.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
-const TestCaseTab = ({ params, input }) => {
+const TestCaseTab = ({ params, input, expected }) => {
   return (
     <div>
       <pre className='case-key'>{params} =</pre>
       <pre className='case-value'>{input}</pre>
+      <pre className='case-key'>Expected =</pre>
+      <pre className='case-value'>{expected}</pre>
     </div>
   );
 };
